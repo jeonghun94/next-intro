d77a725f69493a94130728b9c291fcaa4d4fa616
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-06-16 22:26:17 +0900

    params 사용 디테일 페이지 타이틀 수정

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(`/movies/${title}/${id}`);
+  };
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
@@ -18,8 +31,10 @@ export default function Home({ results }) {
           padding: 20px;
           gap: 20px;
         }
-        .movie img {
+        .movie {
           cursor: pointer;
+        }
+        .movie img {
           max-width: 100%;
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
