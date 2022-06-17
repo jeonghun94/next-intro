6a0b27ac25cbec19cd2e7eaa001742fa58ceaf25
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-06-17 21:55:41 +0900

    404 Page Custom

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Seo from "../components/Seo";
+
+export default function NotFound() {
+  return (
+    <>
+      <Seo title="404 | Movies" />
+      <h1>NotFound Page :(</h1>
+    </>
+  );
+}
